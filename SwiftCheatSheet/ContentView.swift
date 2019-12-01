5940de2bbef3f4bb330f4542486650120cbb02f1
Lee, Bart <LEEB@VALASSIS.COM>
2019-12-01 10:36:49 -0800

    first commit, string

---
+++ SwiftCheatSheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftCheatSheet
+//
+//  Created by Bart Lee on 12/1/19.
+//  Copyright © 2019 NinjaMeowJi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
